6bb1fe85edd7f4e5fc8e4afc56ecda4c32154307
monakhv <monakhv@gmail.com>
2014-04-18 10:47:55 +0400

    new google sync blocking alg

---
+++ src/monakhv/android/samlib/data/ApiClientAsyncTask.java
@@ -0,0 +1,90 @@
+package monakhv.android.samlib.data;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+import java.util.concurrent.CountDownLatch;
+
+/*
+ * Copyright 2014  Dmitry Monakhov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 4/17/14.
+ */
+public abstract class ApiClientAsyncTask  <Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+    private GoogleApiClient mClient;
+
+    public ApiClientAsyncTask(Context context,String account) {
+
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(context)
+                .addApi(Drive.API)
+                .setAccountName(account)
+                .addScope(Drive.SCOPE_FILE)
+                .addScope(Drive.SCOPE_APPFOLDER);
+        mClient = builder.build();
+    }
+
+    @Override
+    protected Result doInBackground(Params... params) {
+        final CountDownLatch latch = new CountDownLatch(1);
+        mClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle bundle) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+
+            }
+        });
+        mClient.registerConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(ConnectionResult connectionResult) {
+                onConnectionFailedTask(connectionResult);
+                latch.countDown();
+            }
+        });
+        mClient.connect();
+        try {
+            latch.await();
+        }catch (InterruptedException ex){
+            return null;
+        }
+        if (!mClient.isConnected()){
+            return null;
+        }
+
+        try {
+            return doInBackgroundConnected(params);
+        }
+        finally {
+            mClient.disconnect();
+        }
+
+    }
+
+    protected GoogleApiClient getGoogleApiClient() {
+        return mClient;
+    }
+    protected abstract Result doInBackgroundConnected(Params... params);
+    public void onConnectionFailedTask(ConnectionResult connectionResult){
+
+    }
+}
--- src/monakhv/android/samlib/data/GoogleDiskOperation.java
+++ src/monakhv/android/samlib/data/GoogleDiskOperation.java
@@ -4,16 +4,14 @@ package monakhv.android.samlib.data;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.os.AsyncTask;
-import android.os.Bundle;
+
 import android.util.Log;
 import android.widget.Toast;
 
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
+
 import com.google.android.gms.drive.Contents;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
@@ -21,6 +19,7 @@ import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
 
 
+import com.google.android.gms.drive.DriveResource;
 import com.google.android.gms.drive.MetadataBuffer;
 import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.drive.query.Filters;
@@ -32,8 +31,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import monakhv.android.samlib.ArchiveActivity;
 import monakhv.android.samlib.R;
@@ -55,19 +53,22 @@ import monakhv.android.samlib.R;
  *
  * 4/1/14.
  */
-public class GoogleDiskOperation extends AsyncTask<Void, Void, Boolean> implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GoogleDiskOperation extends ApiClientAsyncTask<Void, Void, Boolean> {
     public static final int RESOLVE_CONNECTION_REQUEST_CODE = 21;
-    private static final int BUF_SIZE=4096;
+    private static final long TIMEOUT_SEC = 20;
+    private static final int BUF_SIZE = 4096;
 
 
     public enum OperationType {
         EXPORT(R.string.arc_msg_export),
         IMPORT(R.string.arc_msg_import);
         private int iMsg;
-        private OperationType(int i){
-            iMsg=i;
+
+        private OperationType(int i) {
+            iMsg = i;
         }
-        public int getMessage(){
+
+        public int getMessage() {
             return iMsg;
         }
     }
@@ -77,150 +78,70 @@ public class GoogleDiskOperation extends AsyncTask<Void, Void, Boolean> implemen
     private static final String MimeType = "application/octet-stream";
     private Activity context;
     private String account;
-    private GoogleApiClient mGoogleApiClient;
     private OperationType operation;
     private File dataBase;
 
-    public GoogleDiskOperation(Activity ctx, String account,OperationType operationType) {
+    public GoogleDiskOperation(Activity ctx, String account, OperationType operationType) {
+        super(ctx, account);
         this.context = ctx;
         this.account = account;
-        this.operation=operationType;
+        this.operation = operationType;
         dataBase = DataExportImport.getDataBase(ctx);
-        Log.d(DEBUG_TAG,"Original file: "+dataBase.getAbsolutePath());
+       // Log.d(DEBUG_TAG, "Original file: " + dataBase.getAbsolutePath());
 
     }
 
     @Override
-    protected Boolean doInBackground(Void... params) {
-
-
-        mGoogleApiClient = new GoogleApiClient.Builder(context)
-                .addApi(Drive.API)
-                .setAccountName(account)
-                .addScope(Drive.SCOPE_FILE)
-                .addScope(Drive.SCOPE_APPFOLDER)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        try {
-            if (mGoogleApiClient.isConnected()){
-                Log.d(DEBUG_TAG, "Re-Connecting");
-                mGoogleApiClient.reconnect();
-            }
-            else {
-                Log.d(DEBUG_TAG, "Connecting");
-                mGoogleApiClient.connect();
-            }
-
-            return true;
-        } catch (Exception ex) {
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+        if (aBoolean == null) {
             sendResult(false);
-            Log.e(DEBUG_TAG, "Operation " + operation.toString() + " error!", ex);
+            return;
         }
-
-        return null;
+        if (aBoolean) {
+            sendResult(true);
+        } else {
+            sendResult(false);
+        }
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
-        Log.d(DEBUG_TAG, "Get Connected");
-
-        getDriveFile();
-
-    }
-
-    private List<DriveFile> getDriveFile() {
-        final List<DriveFile> res = new ArrayList<DriveFile>();
-        DriveFolder folder = Drive.DriveApi.getAppFolder(mGoogleApiClient);
+    protected Boolean doInBackgroundConnected(Void... params) {
+        DriveFolder folder = Drive.DriveApi.getAppFolder(getGoogleApiClient());
         Query query = new Query.Builder().addFilter(Filters.eq(SearchableField.TITLE, FileName)).build();
-        folder.queryChildren(mGoogleApiClient, query).setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
-            @Override
-            public void onResult(DriveApi.MetadataBufferResult metadataBufferResult) {
-                if (!metadataBufferResult.getStatus().isSuccess()) {
-                    showMessage("Error Search File");
-                    sendResult(false);
-                    return;
-                }
-                MetadataBuffer mdSet = metadataBufferResult.getMetadataBuffer();
+        DriveApi.MetadataBufferResult res = folder.queryChildren(getGoogleApiClient(), query).await(TIMEOUT_SEC, TimeUnit.SECONDS);
 
-                int count = mdSet.getCount();
-                Log.d(DEBUG_TAG, "Found " + count + " files for "+account);
-                switch (operation) {
-                    case EXPORT:
-                        if (count > 0) {
-                            Log.d(DEBUG_TAG, "write to existing file");
-                            writeToFile(Drive.DriveApi.getFile(mGoogleApiClient, mdSet.get(0).getDriveId()));
-                        } else {
-                            Log.d(DEBUG_TAG, "add new  file");
-                            addFile();
-                        }
-                        break;
-                    case IMPORT:
-                        if (count > 0) {
-                            Log.d(DEBUG_TAG, "start reading  file");
-                            readFile(Drive.DriveApi.getFile(mGoogleApiClient, mdSet.get(0).getDriveId()));
-                        }
-                        break;
-
+        if (!res.getStatus().isSuccess()) {
+            showMessage("Error Search File");
+            sendResult(false);
+            return null;
+        }
+        MetadataBuffer mdSet = res.getMetadataBuffer();
+        int count = mdSet.getCount();
+        Log.d(DEBUG_TAG, "Found " + count + " files for " + account);
+        switch (operation) {
+            case EXPORT:
+                if (count > 0) {
+                    Log.d(DEBUG_TAG, "write to existing file");
+                    return writeToFile(Drive.DriveApi.getFile(getGoogleApiClient(), mdSet.get(0).getDriveId()));
+                } else {
+                    Log.d(DEBUG_TAG, "add new  file");
+                    return addFile();
                 }
 
-//                int i = 0;
-//                while (i < count) {
-//                    Metadata md = mdSet.get(i);
-//                    i++;
-//                    res.add(Drive.DriveApi.getFile(mGoogleApiClient, md.getDriveId()));
-//
-//                    Log.d(DEBUG_TAG, md.getTitle()+" - "+md.getModifiedDate().toString()+" - "+md.getDriveId().toString());
-//                }
-
-            }
-        });
-        return res;
-
-
-    }
-
-    /**
-     * Add new file data to Google Drive
-     */
-    private void addFile() {
-        Drive.DriveApi.newContents(mGoogleApiClient).setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
-            @Override
-            public void onResult(DriveApi.ContentsResult contentsResult) {
-                if (!contentsResult.getStatus().isSuccess()) {
-                    showMessage(R.string.res_export_google_bad);
-                    sendResult(false);
-                    return;
+            case IMPORT:
+                if (count > 0) {
+                    Log.d(DEBUG_TAG, "start reading  file");
+                    return readFile(Drive.DriveApi.getFile(getGoogleApiClient(), mdSet.get(0).getDriveId()));
                 }
-                MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                        .setTitle(FileName)
-                        .setMimeType(MimeType)
-                        .setStarred(true).build();
-                // create a file
-                Drive.DriveApi.getAppFolder(mGoogleApiClient)
-                        .createFile(mGoogleApiClient, changeSet, contentsResult.getContents())
-                        .setResultCallback(fileCreateCallback);
+                break;
 
-            }
-        });
-    }
-
-
-    public void showMessage(String message) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-    }
-
-    public void showMessage(int res) {
-        Toast.makeText(context,context.getString(res), Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(DEBUG_TAG, "onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionFailedTask(ConnectionResult connectionResult) {
         Log.d(DEBUG_TAG, "ConnectionFailed Start");
         if (connectionResult.hasResolution()) {
             Log.d(DEBUG_TAG, "ConnectionFailed - has resolution");
@@ -237,91 +158,128 @@ public class GoogleDiskOperation extends AsyncTask<Void, Void, Boolean> implemen
 
     }
 
-    private ResultCallback<DriveFolder.DriveFileResult> fileCreateCallback = new ResultCallback<DriveFolder.DriveFileResult>() {
-        @Override
-        public void onResult(DriveFolder.DriveFileResult driveFileResult) {
-            if (!driveFileResult.getStatus().isSuccess()) {
-                showMessage("Error while trying to create the file");
-                sendResult(false);
-                return;
-            }
-
-            writeToFile(driveFileResult.getDriveFile());
-
-        }
-    };
-
-    private void readFile(final DriveFile file) {
-
-        file.openContents(mGoogleApiClient, DriveFile.MODE_READ_ONLY, null).
-                setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
-                    @Override
-                    public void onResult(DriveApi.ContentsResult contentsResult) {
-                        if (!contentsResult.getStatus().isSuccess()) {
-                            showMessage("Error Reading  file");
-                            sendResult(false);
-                            return;
-                        }
-
-                        InputStream input =contentsResult.getContents().getInputStream();
-                        try {
-                            FileOutputStream output = new FileOutputStream(dataBase);
-                            byte buffer[] = new byte[BUF_SIZE];
-                            int bytesRead;
-                            while ((bytesRead = input.read(buffer)) != -1) {
-                                output.write(buffer, 0, bytesRead);
-                            }
-                            sendResult(true);
-                        }
-                        catch (Exception ex){
-                            showMessage("Error Reading to file");
-                            sendResult(false);
-                        }
-
-                    }
-                });
-    }
 
     /**
-     * Method to write data base to Google Drive
-     * @param file DriveFile
+     * Add new file data to Google Drive
      */
-    private void writeToFile(final DriveFile file) {
-        file.openContents(mGoogleApiClient, DriveFile.MODE_WRITE_ONLY, null).setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
-            @Override
-            public void onResult(DriveApi.ContentsResult contentsResult) {
-                if (!contentsResult.getStatus().isSuccess()) {
-                    showMessage("Error Writing to file");
-                    sendResult(false);
-                    return;
-                }
-                Contents contents = contentsResult.getContents();
-                OutputStream output = contents.getOutputStream();
-                try {
-                    FileInputStream input = new FileInputStream(dataBase);
-                    byte buffer[] = new byte[BUF_SIZE];
-                    int bytesRead;
-                    while ((bytesRead = input.read(buffer)) != -1) {
-                        output.write(buffer, 0, bytesRead);
-                    }
-                } catch (Exception e) {
-                    showMessage("Error Writing to file");
-                    sendResult(false);
+    private boolean addFile() {
+        DriveApi.ContentsResult contentsResult = Drive.DriveApi.newContents(getGoogleApiClient()).await(TIMEOUT_SEC, TimeUnit.SECONDS);
+        if (!contentsResult.getStatus().isSuccess()) {
+            showMessage(R.string.res_export_google_bad);
 
-                }
+            return false;
+        }
+        MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                .setTitle(FileName)
+                .setMimeType(MimeType)
+                .setStarred(true).build();
 
-                file.commitAndCloseContents(mGoogleApiClient, contents).setResultCallback(new ResultCallback<com.google.android.gms.common.api.Status>() {
-                    @Override
-                    public void onResult(com.google.android.gms.common.api.Status status) {
-                        if (!status.isSuccess()) {
-                            showMessage("Error Commit file");
-                            sendResult(false);
-                        }
-                        sendResult(true);
-                    }
-                });
+        Contents originalContents = contentsResult.getContents();
+        OutputStream os = originalContents.getOutputStream();
+        //write data to file
+        try {
+            FileInputStream input = new FileInputStream(dataBase);
+            byte buffer[] = new byte[BUF_SIZE];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
             }
-        });
+        } catch (Exception e) {
+            showMessage("Error Writing to file");
+
+            return false;
+        }
+        // create a file
+        DriveFolder.DriveFileResult fileResult =Drive.DriveApi.getAppFolder(getGoogleApiClient())
+                .createFile(getGoogleApiClient(), changeSet, contentsResult.getContents())
+                .await(TIMEOUT_SEC,TimeUnit.SECONDS);
+
+        if (!fileResult.getStatus().isSuccess()){
+            showMessage("Error saving to file");
+
+            return false;
+        }
+        //reread file
+        DriveResource.MetadataResult metadataResult = fileResult.getDriveFile()
+                .getMetadata(getGoogleApiClient())
+                .await(TIMEOUT_SEC,TimeUnit.SECONDS);
+
+        if (!metadataResult.getStatus().isSuccess()) {
+            showMessage("Error test writing");
+
+            return false;
+        }
+        return true;
+
+    }
+    private boolean writeToFile(DriveFile file ){
+        DriveApi.ContentsResult result = file.openContents(getGoogleApiClient(),DriveFile.MODE_WRITE_ONLY,null)
+                .await(TIMEOUT_SEC, TimeUnit.SECONDS);
+        if (!result.getStatus().isSuccess()){
+            showMessage("Error Writing to file");
+
+            return false;
+        }
+        Contents contents = result.getContents();
+        OutputStream output = contents.getOutputStream();
+        try {
+            FileInputStream input = new FileInputStream(dataBase);
+            byte buffer[] = new byte[BUF_SIZE];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            showMessage("Error Writing to file");
+
+            return false;
+
+        }
+        com.google.android.gms.common.api.Status status = file.commitAndCloseContents(getGoogleApiClient(), contents)
+                .await(TIMEOUT_SEC, TimeUnit.SECONDS);
+        if (!status.isSuccess()){
+            showMessage("Error Commit file");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean readFile(final DriveFile file) {
+
+        DriveApi.ContentsResult result = file.openContents(getGoogleApiClient(), DriveFile.MODE_READ_ONLY, null)
+                .await(TIMEOUT_SEC, TimeUnit.SECONDS);
+        if (   ! result.getStatus().isSuccess()){
+            showMessage("Error Reading  file");
+            sendResult(false);
+            return false;
+        }
+        InputStream input = result.getContents().getInputStream();
+        try {
+            FileOutputStream output = new FileOutputStream(dataBase);
+            byte buffer[] = new byte[BUF_SIZE];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+            sendResult(true);
+            return true;
+        }
+        catch (Exception ex){
+            showMessage("Error Reading to file");
+            sendResult(false);
+            return false;
+        }
+
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void showMessage(int res) {
+        Toast.makeText(context, context.getString(res), Toast.LENGTH_LONG).show();
     }
 
     private void sendResult(boolean res) {
